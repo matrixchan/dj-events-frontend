027f6631d0f75924690f379efdc68d24506effd0
Matrix <immatrixchan@gmail.com>
2021-09-29 17:52:30 +0800

    18 Single Event Page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,42 @@
+import Head from 'next/head'
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import {API_URL} from '@/config/index'
 
-export default function EventsPage() {
+
+export default function EventsPage({events}) {
+
     return (
-        <Layout>
-            <h1>My Events</h1>
+        <Layout >
+            <h1>Events</h1>
+            {events.length === 0 && <h3>No events to show</h3>}
+
+            {events.map((evt)=>(
+            <EventItem key={evt.id} evt={evt}/>
+            ))}
         </Layout>
     )
+}
+
+
+
+// export async function getServerSideProps(){
+//   // console.log(`${API_URL}/api/event`)
+//   const res  = await fetch(`${API_URL}/api/events`)
+//   const events = await res.json()
+
+//   return {
+//     props:{events},
+//   }
+// }
+
+
+export async function getStaticProps(){
+  const res  = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props:{events:events.slice(0,3)},
+    revalidate:1,
+  }
 }
